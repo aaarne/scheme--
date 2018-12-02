252a4cc1ec16d20970df1f308769c0ce08e4ac17
Arne Sachtler <arne.sachtler@gmail.com>
2018-12-02 15:45:36 +0100

    Scheme-- REPL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,18 @@
-import scheme._
+import scheme.Scheme
+import scala.util.{Try, Failure, Success}
 
-object Main extends App {
-  
-  val program = """
-  (def factorial 
-    (lambda (n)
-      (if (= n 0)
-        1
-        (* n (factorial (- n 1)))))
-    (factorial 5))"""
+object Main {
+  import java.io.{BufferedReader, InputStreamReader}
+  val in = new BufferedReader(new InputStreamReader(System.in))
 
-
-  println(s"Program: $program\n")
-
-  val tokens = (for (s <- new ListTokenizer(program)) yield s).toList
-
-  println(s"Tokens: $tokens\n")
-
-  val parsed = Scheme.parseString(program)
-
-  println(s"Parsed: $parsed\n")
-
-  println(s"Decompiled: ${Scheme.decompile(parsed)}")
-
-  val result = Scheme.evaluate(parsed)
-
-  println(s"Evaluated result: $result")
+  def main(args: Array[String]): Unit = {
+    print("lisp> ")
+    in.lines forEach { code => 
+      Try(Scheme.evaluate(code)) match {
+        case Success(result) => println(Scheme.lisp2string(result))
+        case Failure(e) => println(s"Error: $e")
+      }
+      print("lisp> ")
+    }
+  }
 }
--- src/main/scala/scheme/Scheme.scala
+++ src/main/scala/scheme/Scheme.scala
@@ -1,148 +1,220 @@
-package scheme 
+package scheme
 
-class Scheme {
+// scalafix:off
+
+object Scheme {
+
+// Types -------------------------------------------------------
 
   type Data = Any
+
   case class Lambda(f: PartialFunction[List[Data], Data])
 
-  def parseString(s: String): Data = {
-    val it = new ListTokenizer(s)
-    
+// Parsing and Prettyprinting -----------------------------------
+
+  class LispTokenizer(s: String) extends Iterator[String] {
+
+    private var i = 0
+
+    private def isDelimiter(ch: Char) = ch <= ' ' || ch == '(' || ch == ')'
+
+    def hasNext: Boolean = {
+      while (i < s.length && s.charAt(i) <= ' ') {
+        i += 1
+      }
+      i < s.length
+    }
+
+    def next: String =
+      if (hasNext) {
+        val start = i
+        if (isDelimiter(s.charAt(i))) {
+          i += 1
+        }
+        else {
+          do {
+            i += 1
+          } while (i < s.length && !isDelimiter(s.charAt(i)))
+        }
+        s.substring(start, i)
+      } else sys.error("premature end of input")
+  }
+
+  def string2lisp(s: String): Data = {
+
+    val it = new LispTokenizer(s)
+
     def parseExpr(token: String): Data = {
-      if (token == "(") parseList
-      else if (token == ")") throw new RuntimeException("unmatched paranthesis")
-      else if (token.matches("^-?\\d+$")) token.toInt
-      else if (token.charAt(0) == '\"' && token.charAt(token.length-1) == '\"') token.substring(1, token.length-1)
-      else Symbol(token)
+      if (token == "(")
+        parseList
+      else if (token == ")")
+        sys.error("unbalanced parentheses")
+      else if (token.matches("^-?\\d+$"))
+        Integer.parseInt(token)
+      else
+        Symbol(token)
     }
 
     def parseList: List[Data] = {
       val token = it.next
-      if (token == ")") Nil else parseExpr(token) :: parseList
+      if (token == ")")
+        List()
+      else
+        parseExpr(token) :: parseList
     }
-
     parseExpr(it.next)
   }
 
-  def decompile(lisp: Data): String = lisp match {
-    case Symbol(x) => x
-    case xs: List[_] => (xs map decompile).mkString("(", " ", ")")
-    case _ => lisp.toString
+  def lisp2string(x: Data): String = x match {
+    case Symbol(name) =>
+      name
+    case xs: List[_] =>
+      xs.map(lisp2string).mkString("(", " ", ")")
+    case _ =>
+      x.toString
   }
 
+// Diagnostics---------------------------------------------------
+
+  /** Set this to `true` to debug the interpreter */
+  var trace: Boolean = false
+
   var curexp: Data = null
+  var indent: Int = 0
 
   def evaluate(x: Data): Data = eval(x, globalEnv)
 
-  def evaluate(s: String): Data = evaluate(decompile(s))
+  def evaluate(s: String): Data = evaluate(string2lisp(s))
 
   def eval(x: Data, env: Environment): Data = {
+    // Workaround https://github.com/lampepfl/dotty/issues/5544 by using a
+    // non-breakable space instead of a regular space
+    val nonBreakableSpace: String = "\u00a0"
+
     val prevexp = curexp
     curexp = x
-    
+    if (trace) {
+      println((nonBreakableSpace * indent) + s"===> $x")
+      indent += 1
+    }
     val result = eval1(x, env)
+    if (trace) {
+      indent -= 1
+      println((nonBreakableSpace * indent) + s"<=== $result")
+    }
     curexp = prevexp
     result
   }
 
+// Checked conversions ----------- -----------------------------------
+
   def asList(x: Data): List[Data] = x match {
     case xs: List[_] => xs
+    case _ => sys.error(s"expected a list but input was $x")
   }
 
   def paramName(x: Data): String = x match {
     case Symbol(name) => name
+    case _ => sys.error(s"malformed parameter: $x")
   }
 
+// Environments -------------------------------------------------------
 
-abstract class Environment {
-  def lookup(n: String): Data
-  def extend(name: String, v: Data): Environment = {
-    val encl = this
-    new Environment {
-      def lookup(n: String): Data = 
-        if (n == name) v else encl.lookup(n)
+  abstract class Environment {
+    def lookup(n: String): Data
+    def extend(name: String, v: Data): Environment = {
+      val enclosingEnvironment = this
+      new Environment {
+        def lookup(n: String): Data =
+          if (n == name) v else enclosingEnvironment.lookup(n)
+      }
+    }
+    def extendMulti(ps: List[String], vs: List[Data]): Environment = (ps, vs) match {
+      case (List(), List()) => this
+      case (p :: ps1, arg :: args1) => extend(p, arg).extendMulti(ps1, args1)
+      case _ => sys.error("wrong number of arguments")
+    }
+    def extendRec(name: String, expr: Environment => Data): Environment = {
+      val enclosingEnvironment = this
+      new Environment {
+        def lookup(n: String): Data =
+          if (n == name) expr(this)
+          else enclosingEnvironment.lookup(n)
+      }
     }
   }
-  def extendMulti(ps: List[String], vs: List[Data]): Environment = (ps, vs) match {
-    case (Nil, Nil) => this
-    case (p :: ps1, arg :: args1) => extend(p, arg).extendMulti(ps1, args1)
-    case _ => throw new IllegalArgumentException("wrong number of arguments")
-  }
 
-  def extendRec(name: String, expr: Environment => Data): Environment = {
-    val encl = this
-    new Environment {
-      def lookup(n: String): Data =
-        if (n == name) expr(this) else encl.lookup(n)
-    }
+  object EmptyEnvironment extends Environment {
+    def lookup(n: String): Data = sys.error("undefined: " + n + ", this can happen when the wrong number of arguments is passed")
   }
-}
-
-object EmptyEnvironment extends Environment {
-  def lookup(n: String): Data = throw new RuntimeException(s"$n does not exist.")
-}
 
   var globalEnv = EmptyEnvironment
-    .extend("=", Lambda {
-      case List(arg1, arg2) => if (arg1 == arg2) 1 else 0
-    })
-    .extend("+", Lambda {
-      case List(arg1: Int, arg2: Int) => arg1 + arg2
-    })
-    .extend("-", Lambda {
-      case List(arg1: Int, arg2: Int) => arg1 - arg2
-    })
-    .extend("*", Lambda {
-      case List(arg1: Int, arg2: Int) => arg1 * arg2
-    })
-    .extend("/", Lambda {
-      case List(arg1: Int, arg2: Int) => arg1 / arg2
-    })
+    .extend("=", Lambda {case List(arg1, arg2) => if (arg1 == arg2) 1 else 0})
+    .extend("+", Lambda {case List(arg1: Int, arg2: Int) => arg1 + arg2})
+    .extend("-", Lambda {case List(arg1: Int, arg2: Int) => arg1 - arg2})
+    .extend("*", Lambda {case List(arg1: Int, arg2: Int) => arg1 * arg2})
+    .extend("/", Lambda {case List(arg1: Int, arg2: Int) => arg1 / arg2})
     .extend("nil", Nil)
-    .extend("cons", Lambda {
-      case List(arg1, arg2) => arg1 :: asList(arg2)
-    })
-    .extend("car", Lambda {
-      case List(x :: xs) => x
-    })
-    .extend("cdr", Lambda {
-      case List(x :: xs) => xs
-    })
+    .extend("cons", Lambda {case List(arg1, arg2) => arg1 :: asList(arg2)})
+    .extend("car", Lambda {case List(x :: xs) => x})
+    .extend("cdr", Lambda {case List(x :: xs) => xs})
+    .extend("map", Lambda {case List(l: List[Data], f) => l map (e => apply(f, e :: Nil))})
     .extend("null?", Lambda {
       case List(Nil) => 1
       case _ => 0
     })
 
+// Evaluation -----------------------------------
+
   def eval1(x: Data, env: Environment): Data = x match {
     case _: Int => x
     case Symbol(name) => env.lookup(name)
-    case 'val :: Symbol(name) :: expr :: rest :: Nil =>
-      eval(rest, env.extend(name, eval(expr, env)))
-    case 'if :: cond :: thenpart :: elsepart :: Nil =>
-      if (eval(cond, env) != 0) eval(thenpart, env)
-      else (elsepart, env)
+    case 'quote :: y :: Nil => y
+    case 'val :: Symbol(name) :: expr :: rest :: Nil => eval(rest, env.extend(name, eval(expr, env)))
+    case 'lambda :: params :: body :: Nil => mkLambda(asList(params) map paramName, body, env)
     case 'and :: x :: y :: Nil => eval('if :: x :: y :: 0 :: Nil, env)
     case 'or :: x :: y :: Nil => eval('if :: x :: 1 :: y :: Nil, env)
-    case 'def :: Symbol(name) :: body :: Nil if env == globalEnv => 
-      globalEnv = env.extendRec(name, env1 => eval(body, env1))
-    case 'def :: Symbol(name) :: body :: rest :: Nil => 
-      if (env == globalEnv) globalEnv = env.extendRec(name, env1 => eval(body, env1))
-      eval(rest, env.extendRec(name, env1 => eval(body, env1)))
-    case 'quote :: y :: Nil => y
-    case 'lambda :: params :: body :: Nil => 
-      mkLambda(asList(params) map paramName, body, env)
+    case 'case :: scrut :: (('else :: expr :: Nil) :: rest) => eval(expr, env) 
+    case 'if :: cond :: thenpart :: elsepart :: Nil => 
+      if (eval(cond, env) != 0) eval(thenpart, env)
+      else eval(elsepart, env)
+
+    case 'case :: scrut :: ((value :: expr :: Nil) :: rest) => 
+      val evaluated = eval(scrut, env)
+      if (value == evaluated) eval(expr, env)
+      else eval(('case :: evaluated :: Nil) ::: rest, env)
+
+    case 'def :: (name :: args) :: body :: rest :: Nil => 
+      eval('def :: name :: List('lambda, args, body) :: rest :: Nil, env)
+
+    case 'def :: Symbol(name) :: body :: Nil => // definition GLOBAL
+      if (env == globalEnv) {
+        globalEnv = env.extendRec(name, env1 => eval(body, env1))
+        s"def $name" // just confirm we got the def
+      } else {
+        sys.error(s"def $name cannot be added to an inner scope because it is global.\n" +
+                   "Hint: a local def always has three parameters: a name, a body, and the rest of the code to execute")
+      }
+    case 'def :: Symbol(name) :: body :: rest :: Nil => // GLOBAL or LOCAL
+      if (env == globalEnv)
+        globalEnv = env.extendRec(name, env1 => eval(body, env1))
+      eval(rest, env.extendRec(name, env1 => eval(body, env1))) // evaluate
     case operator :: operands =>
-      apply(eval(operator, env), operands map (x => eval(x, env)))
+      try {
+        apply(eval(operator, env), operands.map(x => eval(x, env)))
+      } catch {
+        case ex: MatchError =>
+          sys.error(s"bad arguments for function $operator: $operands")
+      }
   }
 
-  def mkLambda(ps: List[String], body: Data, env: Environment): Lambda = 
-    Lambda {
-      case args => eval(body, env.extendMulti(ps, args))
+  def mkLambda(ps: List[String], body: Data, env: Environment): Lambda =
+    Lambda { case args =>
+      eval(body, env.extendMulti(ps, args))
     }
 
   def apply(f: Data, args: List[Data]): Data = f match {
     case Lambda(f) => f(args)
+    case _ => sys.error(s"application of non-function $f to arguments $args")
   }
 }
-
-object Scheme extends Scheme
